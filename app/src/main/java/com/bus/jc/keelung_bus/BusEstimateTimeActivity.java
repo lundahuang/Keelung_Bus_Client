1bfa374c2b557838150c85c5becad99760cd798c
lundahuang <r275017@gmail.com>
2018-01-08 17:07:55 +0800

    icon
    換icon

--- app/src/main/java/com/bus/jc/keelung_bus/BusEstimateTimeActivity.java
+++ app/src/main/java/com/bus/jc/keelung_bus/BusEstimateTimeActivity.java
@@ -3,6 +3,7 @@ package com.bus.jc.keelung_bus;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -14,29 +15,89 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BusEstimateTimeActivity extends AppCompatActivity {
+    private Bus_Module bus = new Bus_Module();
+    private String Id;
+    private String nameZh;
+    private String departureZh;
+    private String destinationZh;
+    private ListView listviewGo;
+    private ListView listviewBack;
+    private JSONObject json;
+    private Timer timer = new Timer();
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_estimate_time);
+        Intent i = getIntent();
+        Id = i.getStringExtra("Id");
+        nameZh = i.getStringExtra("nameZh");
+        departureZh = i.getStringExtra("departureZh");
+        destinationZh = i.getStringExtra("destinationZh");
 
-    Bus_Module bus = new Bus_Module();
-    JSONObject json;
-    String busStr;
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(nameZh);
 
-    private ListView listEstimateTime;
-    private ArrayAdapter<String> estimateTimeAsapter;
+        TabHost host = (TabHost) findViewById(R.id.tabHost);
+        host.setup();
 
-    private ArrayList<String> getListData(){
-        final ArrayList<String> estimateTimeList = new ArrayList<>();
-        busStr = getIntent().getStringExtra("searchbus");
+        TabHost.TabSpec spec;
+        spec = host.newTabSpec("Tab1");
+        spec.setIndicator("往" + destinationZh);
+        spec.setContent(R.id.Go);
+        host.addTab(spec);
+
+        spec = host.newTabSpec("Tab2");
+        spec.setIndicator("往" + departureZh);
+        spec.setContent(R.id.Back);
+        host.addTab(spec);
+
+        listviewGo = (ListView) findViewById(R.id.listBusdataGo);
+        listviewBack = (ListView) findViewById(R.id.listBusdataBack);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                downloadDataFromServer(true);
+                downloadDataFromServer(false);
+            }
+        };
+
+        timer.schedule(task,0,5000);
+    }
+
+    private void downloadDataFromServer(boolean goBack) {
+        final boolean goBack_f = goBack;
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    json = bus.busRouteEstimateTime(busStr,true);
-                    JSONArray jsonArray = json.getJSONArray("routeInfo");
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        String info = jsonArray.getJSONObject(i).getString("nameZh") + jsonArray.getJSONObject(i).getString("EstimateTime");
-                        estimateTimeList.add(info);
+                    json = bus.busRouteEstimateTime(Id,goBack_f);
+                    System.out.println(json);
+                    final ArrayList<String> str = new ArrayList<String>();
+                    JSONArray array = ((JSONArray)json.get("routeInfo"));
+                    if(array.length() == 0 )
+                        str.add("沒有這個方向的資料！");
+                    for(int i = 0 ; i < array.length() ; i++) {
+                        JSONObject data = array.getJSONObject(i);
+                        str.add(data.getString("nameZh") + " - " + data.getString("EstimateTime"));
                     }
+
+                    runOnUiThread(new Runnable(){
+                        @Override
+                        public void run() {
+                            ArrayAdapter adapter = new ArrayAdapter(BusEstimateTimeActivity.this,android.R.layout.simple_list_item_1,str);
+                            if(goBack_f)
+                                listviewGo.setAdapter(adapter);
+                            else
+                                listviewBack.setAdapter(adapter);
+                        }
+                    });
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (JSONException e) {
@@ -45,32 +106,5 @@ public class BusEstimateTimeActivity extends AppCompatActivity {
             }
         });
         thread.start();
-
-        return estimateTimeList;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bus_estimate_time);
-
-        TabHost host = (TabHost) findViewById(R.id.tabHost);
-        host.setup();
-
-        TabHost.TabSpec spec;
-        spec = host.newTabSpec("Tab1");
-        spec.setIndicator("往新豐街");
-        spec.setContent(R.id.Go);
-        host.addTab(spec);
-
-        spec = host.newTabSpec("Tab2");
-        spec.setIndicator("往總站");
-        spec.setContent(R.id.Back);
-        host.addTab(spec);
-
-        ArrayList<String> list = getListData();
-        estimateTimeAsapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-        listEstimateTime  = (ListView) findViewById(R.id.listBusdata);
-        listEstimateTime.setAdapter(estimateTimeAsapter);
     }
 }
--- app/src/main/java/com/bus/jc/keelung_bus/MainActivity.java
+++ app/src/main/java/com/bus/jc/keelung_bus/MainActivity.java
@@ -1,23 +1,16 @@
 package com.bus.jc.keelung_bus;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    Bus_Module bus = new Bus_Module();
-    JSONObject json;
-
-    private Button busbtn;
-    private Button stopbtn;
+    EditText searchTextField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,49 +19,20 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("基隆公車Only");
+        getSupportActionBar().setTitle("基隆公車搜尋");
 
-        busbtn = (Button) findViewById(R.id.busbtn);
+        searchTextField = (EditText)findViewById(R.id.editText);
+
+        Button busbtn = (Button) findViewById(R.id.searchButton);
         busbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent();
-                i.setClass(MainActivity.this, SearchBusActivity.class);
+                i.putExtra("KeyWords",searchTextField.getText().toString());
+                i.setClass(MainActivity.this, SearchListActivity.class);
                 startActivityForResult(i,0);
             }
         });
 
-        stopbtn = (Button) findViewById(R.id.stopbtn);
-        stopbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent();
-                i.setClass(MainActivity.this, SearchStopActivity.class);
-                startActivityForResult(i,0);
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    json = bus.searchBus("海大");
-                    System.out.println(json);
-                    json = bus.busStopEstimateTime("情人湖濱海大道紀念碑",true);
-                    System.out.println(json);
-                    json = bus.busRouteEstimateTime("3052",true);
-                    System.out.println(json);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        thread.start();
     }
 }
